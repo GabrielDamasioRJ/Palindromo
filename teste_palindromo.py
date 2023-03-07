0ff9a0c4e85804f90413350daf6ae3d4c11e617a
Gabriel Damasio Evangelista <108025144+GabrielDamasioRJ@users.noreply.github.com>
2023-03-07 01:24:10 -0200

    Add files via upload

---
+++ teste_palindromo.py
@@ -0,0 +1,18 @@
+palin = str(input("Digite uma frase: "))
+
+s=palin.replace(' ','')
+sreverse=s[::-1];
+
+
+
+if s==sreverse:
+        print(" é palíndromo ")
+else:
+
+        print(" não é palíndromo")
+
+
+
+
+
+
